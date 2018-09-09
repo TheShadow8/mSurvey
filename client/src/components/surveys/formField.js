750c946a55d9f8ee7750764d3f2367237b6a4191
Minh Do <minhdo.ca@gmail.com>
2018-09-09 19:58:58 -0400

    add tips

--- client/src/components/surveys/formField.js
+++ client/src/components/surveys/formField.js
@@ -2,5 +2,9 @@ export default [
   { label: 'Survey Title', name: 'title' },
   { label: 'Subject Line', name: 'subject' },
   { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'recipients' },
+  {
+    label:
+      'Recipient List (You can input multiple email. Ex: a@mail.com, b@mail.com )',
+    name: 'recipients',
+  },
 ];
